e28757183190dc5ab81ac4158205f88ef329dd24
lathrop <daniel.lathrop@gmail.com>
2015-02-05 20:50:07 -0600

    fiddling

--- listener/views.py
+++ listener/views.py
@@ -4,7 +4,7 @@ from models import Heard
 
 
 def listen(request):
-    h = Heard(payload=request)
+    h = Heard(payload=request.body)
     h.save()
     hub_token = request.GET.get('hub.challenge')
     return HttpResponse(hub_token)
