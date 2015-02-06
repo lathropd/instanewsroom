acee2996e4f450c3201e3a495d225463ebc18ff6
lathrop <daniel.lathrop@gmail.com>
2015-02-05 21:21:12 -0600

    fiddling

--- listener/views.py
+++ listener/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import Heard
-
+import json
 
 def listen(request):
-    for item in request.body:
+    for item in json.loads(request.body):
         h = Heard(payload=item)
-    h.save()
+        h.save()
     hub_token = request.GET.get('hub.challenge')
     return HttpResponse(hub_token)
